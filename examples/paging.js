19d2ad5112d0704b39d6b679faa911615e8f8ede
Tim Santeford <tsantef@gmail.com>
2015-01-30 15:37:59 -0800

    Added paging example

---
+++ examples/paging.js
@@ -0,0 +1,38 @@
+var Api = require('../lib/api');
+var Promise = require('q').Promise;
+
+var api = new Api({
+	target: process.env.TARGET,
+	token: 'SESSION_TOKEN'
+});
+
+function getAllCurrencies() {
+	return new Promise(function (resolve, reject) {
+		var currencies = [];
+
+		function _page(startElement) {
+			return api.getJson({
+				uri: 'currency',
+				startElement: startElement,
+				numElements: 5
+			}).then(function (res) {
+				var response = res.body.response;
+				currencies = currencies.concat(response.currencies);
+
+				if (startElement >= response.count) {
+					return resolve(currencies);
+				} else {
+					return _page(startElement + response.num_elements);
+				}
+			}).catch(reject);
+		}
+
+		_page(0);
+	});
+}
+
+return getAllCurrencies().then(function (currencies) {
+	console.log(currencies);
+}).catch(function (err) {
+	console.log(err);
+});
