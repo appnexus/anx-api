5d9fd35217406459ca0f107e5aa8b2a57ebd2bc9
Dan Van Horn <dan.vanhorn17@gmail.com>
2018-08-03 09:59:34 -0700

    refactored some modules to ES6 syntax

--- src/request-queue.ts
+++ src/request-queue.ts
@@ -9,110 +9,118 @@ const ONE_SECOND = 1000;
 const DEFAULT_RATE_LIMIT_TIMEOUT = DEFAULT_LIMIT_SECONDS * ONE_SECOND;
 const RETRY_AFTER_BUFFER_TIME = ONE_SECOND;
 
-function RequestQueue(options) {
-	this.options = _.assign({
-		request: null,
-		limit: DEFAULT_LIMIT,
-		limitSeconds: DEFAULT_LIMIT_SECONDS,
-		onRateLimitExceeded: _.noop,
-		onRateLimitPause: _.noop,
-		onRateLimitResume: _.noop,
-	}, options);
-	this.queue = [];
-	this.limitCount = 0;
-	this._resetTimeout();
+export interface IRequestQueueOptions {
+	request: any;
+	limit: number;
+	limitSeconds: number;
+	limitHeader: any;
+	onRateLimitExceeded: () => void;
+	onRateLimitPause: () => void;
+	onRateLimitResume: () => void;
 }
 
-RequestQueue.prototype.enqueue = function _enqueue(opts) {
-	const _self = this;
-	return new Promise(function queuedPromise(resolve, reject) {
-		_self.queue.push({
-			opts,
-			resolve,
-			reject,
+export class RequestQueue {
+	private options: IRequestQueueOptions;
+	private queue: any[];
+	private limitCount: number;
+	private expires: number;
+	private timeoutId: any;
+
+	constructor(options: IRequestQueueOptions){
+		this.options = _.assign({
+			request: null,
+			limit: DEFAULT_LIMIT,
+			limitSeconds: DEFAULT_LIMIT_SECONDS,
+			onRateLimitExceeded: _.noop,
+			onRateLimitPause: _.noop,
+			onRateLimitResume: _.noop,
+		}, options);
+		this.queue = [];
+		this.limitCount = 0;
+		this._resetTimeout();
+	}
+
+	public enqueue(opts) {
+		return new Promise((resolve, reject) => {
+			this.queue.push({
+				opts,
+				resolve,
+				reject,
+			});
+			this._processQueue();
 		});
-		_self._processQueue();
-	});
-};
+	}
 
-RequestQueue.prototype.dequeue = function _dequeue() {
-	const _self = this;
-	return _self.queue.shift();
-};
+	public dequeue() {
+		return this.queue.shift();
+	}
 
-RequestQueue.prototype.paused = function paused() {
-	const _self = this;
-	return !!_self.timeoutId;
-};
+	public paused() {
+		return !!this.timeoutId;
+	};
 
-RequestQueue.prototype._processQueue = function _processQueue(retryAfter) {
-	const _self = this;
-	if (_self.queue.length > 0) { // if items left to process
-		if (_self.limitCount < Math.max(_self.options.limit - DEFAULT_LIMIT_COUNT_BUFFER, 1)) { // if not over limit
-			_self.limitCount++;
-			_self._execute(_self.dequeue());
-			_self._processQueue();
-		} else if (!_self.paused()) { // schedule
-			_self.options.onRateLimitPause();
-			_self._schedule(retryAfter);
+	private _processQueue(retryAfter?) {
+		if (this.queue.length > 0) { // if items left to process
+			if (this.limitCount < Math.max(this.options.limit - DEFAULT_LIMIT_COUNT_BUFFER, 1)) { // if not over limit
+				this.limitCount++;
+				this._execute(this.dequeue());
+				this._processQueue();
+			} else if (!this.paused()) { // schedule
+				this.options.onRateLimitPause();
+				this._schedule(retryAfter);
+			}
 		}
 	}
-};
 
-RequestQueue.prototype._schedule = function _schedule(retryAfter) {
-	const _self = this;
-	if (!this.timeoutId) {
-		const delay = Math.max(retryAfter || (this.expires - Date.now()), 0);
-		_self.timeoutId = setTimeout(function scheduleRun() {
-			_self._resetTimeout();
-			_self.limitCount = 0;
-			_self.options.onRateLimitResume();
-			_self._processQueue();
-		}, delay);
+	private _schedule(retryAfter?) {
+		if (!this.timeoutId) {
+			const delay = Math.max(retryAfter || (this.expires - Date.now()), 0);
+			this.timeoutId = setTimeout(() => {
+				this._resetTimeout();
+				this.limitCount = 0;
+				this.options.onRateLimitResume();
+				this._processQueue();
+			}, delay);
+		}
 	}
-};
 
-RequestQueue.prototype._resetTimeout = function _resetTimeout() {
-	const _self = this;
-	if (this.timeoutId) {
-		clearTimeout(_self.timeoutId);
-		_self.timeoutId = null;
-	}
-	_self.expires = Date.now() + ((_self.options.limitSeconds + DEFAULT_LIMIT_SECONDS_BUFFER) * ONE_SECOND);
-};
+	private _resetTimeout() {
+		if (this.timeoutId) {
+			clearTimeout(this.timeoutId);
+			this.timeoutId = null;
+		}
+		this.expires = Date.now() + ((this.options.limitSeconds + DEFAULT_LIMIT_SECONDS_BUFFER) * ONE_SECOND);
+	};
 
-RequestQueue.prototype._execute = function _execute(reqInfo) {
-	const _self = this;
-	return _self.options.request(reqInfo.opts).then(function success(res) {
-		_self._checkHeaders(res);
-		return reqInfo.resolve(res);
-	}).catch(function failure(err) {
-		if (err instanceof errors.RateLimitExceededError) {
-			_self.options.onRateLimitExceeded(err);
-			if (_.isNil(err.retryAfter)) {
-				// Abort retry due to missing retryAfter
+	private _execute(reqInfo) {
+		return this.options.request(reqInfo.opts).then(function success(res) {
+			this._checkHeaders(res);
+			return reqInfo.resolve(res);
+		}).catch(function failure(err) {
+			if (err instanceof errors.RateLimitExceededError) {
+				this.options.onRateLimitExceeded(err);
+				if (_.isNil(err.retryAfter)) {
+					// Abort retry due to missing retryAfter
+					return reqInfo.reject(err);
+				}
+				const retryAfter = err.retryAfter ? (err.retryAfter * ONE_SECOND) + RETRY_AFTER_BUFFER_TIME : DEFAULT_RATE_LIMIT_TIMEOUT;
+				this.limitCount = Infinity;
+				this.queue.push(reqInfo);
+				this._processQueue(retryAfter);
+			} else {
 				return reqInfo.reject(err);
 			}
-			const retryAfter = err.retryAfter ? (err.retryAfter * ONE_SECOND) + RETRY_AFTER_BUFFER_TIME : DEFAULT_RATE_LIMIT_TIMEOUT;
-			_self.limitCount = Infinity;
-			_self.queue.push(reqInfo);
-			_self._processQueue(retryAfter);
-		} else {
-			return reqInfo.reject(err);
-		}
-	});
-};
+		});
+	}
 
-RequestQueue.prototype._checkHeaders = function _checkHeaders(res) {
-	const _self = this;
-	if (res.headers[_self.options.limitHeader]) {
-		const limit = parseInt(res.headers[_self.options.limitHeader], 10) || DEFAULT_LIMIT;
-		if (limit !== _self.options.limit) {
-			_self.options.limit = limit;
-			_self._resetTimeout();
-			_self._processQueue();
+	private _checkHeaders(res) {
+		if (res.headers[this.options.limitHeader]) {
+			const limit = parseInt(res.headers[this.options.limitHeader], 10) || DEFAULT_LIMIT;
+			if (limit !== this.options.limit) {
+				this.options.limit = limit;
+				this._resetTimeout();
+				this._processQueue();
+			}
 		}
 	}
-};
-
-export default RequestQueue;
+}
